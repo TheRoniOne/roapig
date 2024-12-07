9b28f7657d5c539b16abd88666d87b090afcc908
TheRoniOne <ronidani99@gmail.com>
2024-12-07 18:39:07 -0500

    Add basic functionality to load files from a project

--- src/reader.rs
+++ src/reader.rs
@@ -1,3 +1,38 @@
-pub fn load_and_parse(input: &str) -> Vec<Router> {
-    vec![]
+use std::fs;
+
+use walkdir::{DirEntry, WalkDir};
+
+use crate::types::{
+    module_name::ModuleName, openapi_spec::OpenApiSpec3_1, router::Router, structure::Structure,
+};
+
+pub fn load_and_parse(root_path: &str) -> OpenApiSpec3_1 {
+    let routers: Vec<Router> = vec![];
+    let structures: Vec<Structure> = vec![];
+    let module_names: Vec<ModuleName> = vec![];
+
+    let walker = WalkDir::new(root_path).into_iter();
+    for entry in walker.filter_entry(|e| !is_hidden(e)) {
+        match entry {
+            Ok(entry) => {
+                if entry.file_type().is_file() {
+                    println!("Found file: {}", entry.path().display());
+
+                    let file_contents = fs::read_to_string(entry.path()).unwrap();
+                } else if entry.file_type().is_dir() {
+                    println!("Found directory: {}", entry.path().display());
+                }
+            }
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+    todo!()
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
 }
